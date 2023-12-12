7e31782edb71f0bb2d6cb4fd882b9122a5eb7b58
hyeonpearl <hyeoncheol.contact@gmail.com>
2023-12-12 21:16:02 +0900

    Init: 프로젝트 리액트로 초기화

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      Challenge by{' '}
+      <a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+        Frontend Mentor
+      </a>
+      . Coded by <a href='https://github.com/hyeonpearl'>HyeonCheol</a>.
+    </footer>
+  );
+}
